b2aa204a3f2df0a017ad5a0bb3826279ba499a28
MultiMote <sporeon@yandex.ua>
2015-01-24 21:15:10 +0300

    Removed shadow in invincibility mode

--- src/main/java/com/multi/icyadmin/handlers/ClientHandler.java
+++ src/main/java/com/multi/icyadmin/handlers/ClientHandler.java
@@ -2,6 +2,8 @@ package com.multi.icyadmin.handlers;
 
 import com.multi.icyadmin.data.CapabilitiesEnum;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.ReflectionHelper;
+import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
@@ -10,6 +12,8 @@ import net.minecraftforge.client.event.RenderPlayerEvent;
  * Created by MultiMote on 09.01.2015.
  */
 public class ClientHandler {
+    boolean invPrev;
+
     @SubscribeEvent
     public void renderNamePlate(RenderLivingEvent.Specials.Pre e) {
         if (e.entity instanceof EntityPlayer && PlayerProps.get(((EntityPlayer) e.entity)).isPropEnabled(CapabilitiesEnum.HIDE_NAME)) {
@@ -19,7 +23,18 @@ public class ClientHandler {
 
     @SubscribeEvent
     public void renderPlayer(RenderPlayerEvent.Pre e) {
-        if (PlayerProps.get(e.entityPlayer).isPropEnabled(CapabilitiesEnum.INVINCIBILITY)) {
+        boolean inv = PlayerProps.get(e.entityPlayer).isPropEnabled(CapabilitiesEnum.INVINCIBILITY);
+
+        if (invPrev != inv) {
+            try {
+                ReflectionHelper.setPrivateValue(Render.class, e.renderer, inv ? 0.0F : 1.0F, "shadowOpaque", "field_76987_f");
+            } catch (ReflectionHelper.UnableToAccessFieldException ex) {
+                ex.printStackTrace();
+            }
+            invPrev = inv;
+        }
+
+        if (inv) {
             e.setCanceled(true);
         }
     }
